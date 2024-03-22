82ad92d0063aa6e84e91455b970d4425c227f2ee
Craig Richards <craig@greywidget.co.uk>
2024-03-22 17:44:10 +0000

    Temporary Change to 5 minute loop

--- notify/main.py
+++ notify/main.py
@@ -19,6 +19,7 @@ from typing_extensions import Annotated
 
 DEFAULT_TAG = "snake"
 FIFTEEN_MINUTES = 15 * 60
+FIVE_MINUTES = 5 * 60
 HEARTBEAT = "white_check_mark"
 LOG_FILE = Path(__file__).resolve().parent.parent / "notify.log"
 
@@ -91,7 +92,7 @@ def run():
             publish(f"{today}", priority=1, tag=HEARTBEAT)
             last_heartbeat = today
 
-        sleep(FIFTEEN_MINUTES)
+        sleep(FIVE_MINUTES)
 
 
 if __name__ == "__main__":
